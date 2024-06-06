d8ab409213aa0ad98ef6b3d116a5fa606d69cdad
Mahsa <mahsa.ghezel.1995@gmail.com>
2024-06-06 22:12:48 +0330

    added the dark mode

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,6 +8,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
+import ThemeToggler from './ThemeToggler';
 
 const Navbar = () => {
   return (
@@ -16,28 +17,30 @@ const Navbar = () => {
         <Link href='/' className='text-lg'>
           Mahsa's Dashboard
         </Link>
-
-        <DropdownMenu>
-          <DropdownMenuTrigger className='focus:outline-none'>
-            <Avatar>
-              <AvatarImage
-                src='https://images.unsplash.com/photo-1518288774672-b94e808873ff?q=80&w=1938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-                alt='Profile pic'
-              />
-              <AvatarFallback className='text-black'>MD</AvatarFallback>
-            </Avatar>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>
-              <Link href='/profile'>Profile</Link>
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              <Link href='/auth'>Logout</Link>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <div className='flex items-center'>
+          <ThemeToggler />
+          <DropdownMenu>
+            <DropdownMenuTrigger className='focus:outline-none'>
+              <Avatar>
+                <AvatarImage
+                  src='https://images.unsplash.com/photo-1518288774672-b94e808873ff?q=80&w=1938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+                  alt='Profile pic'
+                />
+                <AvatarFallback className='text-black'>MD</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <Link href='/profile'>Profile</Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Link href='/auth'>Logout</Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
       </div>
     </>
   );
