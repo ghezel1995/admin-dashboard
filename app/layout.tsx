cf1d26543fc637b28740d4acd0922dfc2d24764d
Mahsa <mahsa.ghezel.1995@gmail.com>
2024-06-06 21:47:16 +0330

    fixed the sidebar height problem

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar />
         <div className='flex'>
-          <div className='hidden md:block h-[100vh] w-[300px]'>
+          <div className='hidden md:block h-[155vh] w-[300px]'>
             <Sidebar />
           </div>
           <div className='p-5 w-full md:max-w-[1140px]'>{children}</div>
